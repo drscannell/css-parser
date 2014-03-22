bece5fea878d709c27f9a443103267a1be14bb25
drscannellrh <dscannell@randomhouse.com>
2014-03-22 17:31:25 -0400

    add test for comment in value

--- src/test/test_css_parser.py
+++ src/test/test_css_parser.py
@@ -43,7 +43,8 @@ class TestCases:
 			'test_06_comment_in_selector.txt',
 			'test_07_comment_after_declaration.txt',
 			'test_08_comment_after_blockstart.txt',
-			'test_09_commented_declaration.txt']
+			'test_09_commented_declaration.txt',
+			'test_10_comment_in_value.txt']
 
 		for filename in testfiles:
 			yield self.check_tokenize, filename
