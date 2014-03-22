c0f5c025954b0626101aa5cfac1863cf656519e9
drscannellrh <dscannell@randomhouse.com>
2014-03-22 13:48:41 -0400

    update tests

--- src/test/test_css_parser.py
+++ src/test/test_css_parser.py
@@ -1,10 +1,6 @@
 from css_parser import CssTokenizer
 
-tokens = CssTokenizer.tokenize('/*')
-assert len(tokens) == 1
-assert tokens[0].tokentype == '<comment>'
-
-class MyTest:
+class TokenizerTest:
 	def __init__(self,txt,expected):
 		self.txt = txt
 		self.expected = expected
@@ -14,44 +10,22 @@ class MyTest:
 
 class TestCases:
 	def test_tokenize(self):
-		tests = []
-		tests.append(MyTest('/* comment*/',
-			['<comment>','<content/>','</comment>']))
-		tests.append(MyTest('/* comment*/\nspan{margin:0;}',
-			['<comment>','<content/>','</comment>','<selector/>',
-				'<block>','<property/>','<value/>','</block>']))
-		for test in tests:
-			yield self.check_tokenize, test
+		args = [
+			('/* comment*/',
+				['/*','txt','*/']),
+			('/* comment*/\nspan{margin:0;}',
+				['/*','txt','*/','selector',
+				'{','prop','val','}'])
+				]
+
+		for args_tuple in args:
+			yield self.check_tokenize, args_tuple
 	
-	def check_tokenize(self, test):
-		print 'input: %s' % (test.txt)
-		print 'expected: %s' % (test.expected)
+	def check_tokenize(self, arg_tuple):
+		txt, expected = arg_tuple
+		test = TokenizerTest(txt, expected)
+		print 'input: %s' % (txt)
+		print 'expected: %s' % (expected)
 		print 'observed: %s' % ([t.tokentype for t in test.tokens])
 		test.test()
-
-txt = '''/* sample stylesheet */
-p.indent {
-	text-indent:0em;
-	margin: 0 1em 0 1em;
-}
-
-div.hooray
-{
-	font-family: Helvetica, sans-serif;
-} /* remember the alamo */
-
-/*
- * and remember
- * to brush your
- * teeth
- */
- '''
-
-'''
-print ''
-tokens = CssTokenizer.tokenize(txt)
-for t in tokens:
-	print '%s\t\t%s' % (t.tokentype, t.tokentext)
-print ''
-'''
 
