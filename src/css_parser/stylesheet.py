2dacb17dddc0810d18e4350b0232b276316faa62
drscannellrh <dscannell@randomhouse.com>
2014-03-27 10:27:45 -0400

    add StyleSheet.append_rule & insert_rule_after

--- src/css_parser/stylesheet.py
+++ src/css_parser/stylesheet.py
@@ -13,6 +13,22 @@ class StyleSheet:
 
 	def set_rules(self, rules):
 		self.rules = rules
+
+	def append_rule(self, newrule):
+		self.tokens += newrule.get_tokens()
+		self.rules.append(newrule)
+
+	def insert_rule_after(self, newrule, existingrule):
+		existingruletokens = existingrule.get_tokens()
+		lasttoken = existingruletokens[len(existingruletokens) - 1]
+		if lasttoken == self.tokens[-1]:
+			self.tokens += newrule.get_tokens()
+		else:
+			i = self.tokens.index(lasttoken) + 1
+			for t in reversed(newrule.get_tokens()):
+				self.tokens.insert(i, t)
+		i = self.rules.index(existingrule)
+		self.rules.insert(i, newrule)
 	
 	def get_rules(self):
 		return self.rules
--- src/tests/test_stylesheet.py
+++ src/tests/test_stylesheet.py
@@ -2,6 +2,8 @@ import os
 import re
 from css_parser.tokenizer import Tokenizer
 from css_parser.stylesheet import StyleSheet
+from css_parser.stylesheet_reader import StyleSheetReader
+from css_parser.rulefactory import RuleFactory
 
 class TestCases:
 
@@ -40,4 +42,74 @@ class TestCases:
 		print 'expected:\n%s' % (txt_in)
 		print '\nobserved:\n%s' % (txt_out)
 		assert txt_in == txt_out
+
+	# ----------------------------------------
+
+	def test_append_rule(self):
+		tests = [
+				{'input': 'body{margin:0;}',
+				 'to_append': 'p{text-indent:1em;}',
+				 'expected_num_rules': 2,
+				 'expected_string': 'body{margin:0;}p{text-indent:1em;}'},
+				{'input': 'body{margin:0;}/* comment */',
+				 'to_append': 'p{text-indent:1em;}',
+				 'expected_num_rules': 2,
+				 'expected_string': 'body{margin:0;}/* comment */p{text-indent:1em;}'}
+				]
+
+		for test in tests:
+			yield self.check_append_rule, test
+	
+	def check_append_rule(self, test):
+		stylesheet = StyleSheetReader.read_string(test['input'])
+		new_rule_tokens = Tokenizer.tokenize_string(test['to_append'])
+		new_rule = RuleFactory.construct(new_rule_tokens)
+		stylesheet.append_rule(new_rule)
+		observed_num_rules = len(stylesheet.get_rules())
+		observed_string = str(stylesheet)
+		for k in test:
+			print '%s: %s' % (k, test[k])
+		print 'observed_num_rules: %s' % (observed_num_rules)
+		print 'observed_string: %s' % (observed_string)
+		assert observed_num_rules == test['expected_num_rules']
+		assert observed_string == test['expected_string']
+
+	# ----------------------------------------
+
+	def test_insert_rule_after(self):
+		tests = [
+				{'input': 'body{margin:0;}',
+				 'insert_after_index': 0,
+				 'to_insert': 'p{text-indent:1em;}',
+				 'expected_num_rules': 2,
+				 'expected_string': 'body{margin:0;}p{text-indent:1em;}'},
+				{'input': 'body{margin:0;}/* comment */',
+				 'insert_after_index': 0,
+				 'to_insert': 'p{text-indent:1em;}',
+				 'expected_num_rules': 2,
+				 'expected_string': 'body{margin:0;}p{text-indent:1em;}/* comment */'}
+				]
+
+		for test in tests:
+			yield self.check_insert_rule_after, test
+	
+	def check_insert_rule_after(self, test):
+		stylesheet = StyleSheetReader.read_string(test['input'])
+		new_rule_tokens = Tokenizer.tokenize_string(test['to_insert'])
+		new_rule = RuleFactory.construct(new_rule_tokens)
+		existing_rule = stylesheet.get_rules()[test['insert_after_index']]
+		stylesheet.insert_rule_after(new_rule, existing_rule)
+		observed_num_rules = len(stylesheet.get_rules())
+		observed_string = str(stylesheet)
+		for k in test:
+			print '%s: %s' % (k, test[k])
+		print 'observed_num_rules: %s' % (observed_num_rules)
+		print 'observed_string: %s' % (observed_string)
+		assert observed_num_rules == test['expected_num_rules']
+		assert observed_string == test['expected_string']
+
+
+
+
+
 
